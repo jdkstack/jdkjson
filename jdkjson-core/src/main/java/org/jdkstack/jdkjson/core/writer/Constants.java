1785c1e31ccfbd7922fcc9a31bfcdcacc7045bd5
admin <www.sanshiwu.com@gmail.com>
2021-08-16 22:41:14 +0800

    feat(core): core
    core
    
    BREAKING CHANGE: core

--- jdkjson-core/src/main/java/org/jdkstack/jdkjson/core/writer/Constants.java
+++ jdkjson-core/src/main/java/org/jdkstack/jdkjson/core/writer/Constants.java
@@ -8,15 +8,6 @@ package org.jdkstack.jdkjson.core.writer;
  * @author admin
  */
 public final class Constants {
-  /** LRU最大缓存10000 . */
-  public static final int CAPACITY = 10000;
-  /** . . */
-  public static final String FULL_STOP = ".";
-
-  /** . */
-  public static final int HASH_MAP = 16;
-  /** . */
-  public static final int LIST = 10;
 
   private Constants() {
     //
