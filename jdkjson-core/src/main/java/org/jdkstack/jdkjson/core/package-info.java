dd8e379ad632c6315887b4016eae79c4bbada3db
admin <jdkstack@gmail.com>
2021-08-22 16:34:50 +0800

    feat(core): core
    core
    
    BREAKING CHANGE: core

--- jdkjson-core/src/main/java/org/jdkstack/jdkjson/core/package-info.java
+++ jdkjson-core/src/main/java/org/jdkstack/jdkjson/core/package-info.java
@@ -1,7 +1,42 @@
 /**
- * .
+ * JSON的处理.
  *
- * <p>.
+ * <p>JSON的处理包括几个部分.
+ *
+ * <p>JSON 数据绑定.是在反序列化和序列化的过程中进行映射的操作.
+ *
+ * <PRE>
+ *    数字映射成整形还是浮点型.
+ *    字符串映射成日期类型还是电子邮件类型.
+ *  </PRE>
+ *
+ * <p>JSON 反序列化.反序列化是将json字符串转换成json对象之前的过程.
+ *
+ * <PRE>
+ *   之后会进行JSON数据绑定操作.
+ * </PRE>
+ *
+ * <p>JSON 序列化.序列化是将json对象转换成json字符串之前的过程.
+ *
+ * <PRE>
+ *   之后会进行JSON数据绑定操作.
+ * </PRE>
+ *
+ * <p>JSON转换.
+ *
+ * <PRE>
+ *    JSON2PATCH.
+ *    PATCH2JSON.
+ *    JSON-MERGER)
+ *  </PRE>
+ *
+ * <p>JSON管理.
+ *
+ * <PRE>
+ *    JSON-PATH.
+ *    JSON-POINTER.
+ *    JSON-SCHEME.
+ *  </PRE>
  *
  * @author admin
  */
